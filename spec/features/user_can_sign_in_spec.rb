6ad6310e32101f944719e815a252213a56c7cf14
Quinn Lee <leekeunhwi@gmail.com>
2013-06-18 06:51:29 -0400

    changed root to movies#index

--- spec/features/user_can_sign_in_spec.rb
+++ spec/features/user_can_sign_in_spec.rb
@@ -6,7 +6,7 @@ feature "user will login" do
 
 scenario "user already has account, wants to login" do
   visit(root_path)
-  click_link("login")
+  click_link("Login")
   fill_in 'Email', :with => user.email
   fill_in 'Password', :with => user.password
   click_button("Sign in")
