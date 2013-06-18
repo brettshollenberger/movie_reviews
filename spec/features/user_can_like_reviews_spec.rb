81df16037f3d3bdc8c5fb76c0f634ddcdcf0fd4d
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-06-18 14:45:44 -0400

    By comment I meant review

--- spec/features/user_can_like_reviews_spec.rb
+++ spec/features/user_can_like_reviews_spec.rb
@@ -3,22 +3,22 @@ require 'spec_helper'
 include Warden::Test::Helpers
 Warden.test_mode!
 
-feature "user can like a comment" do
+feature "user can like a review" do
 
   background do
-    @comment = FactoryGirl.create(:comment)
+    @review = FactoryGirl.create(:review)
     @user = FactoryGirl.create(:user)
     login_as(@user, :scope => :user)
-    visit movie_path(@comment.movie)
+    visit movie_path(@review.movie)
   end
 
-  scenario "user hasn't liked the comment" do
+  scenario "user hasn't liked the review" do
     expect(page).to_not have_content("1 like")
     click_link "movie[like]"
     expect(page).to have_content("1 like")
   end
 
-  scenario "user has liked the comment already" do
+  scenario "user has liked the review already" do
     click_link "movie[like]"
     expec(page).to have_content("1 like")
     click_link "movie[unlike]"
