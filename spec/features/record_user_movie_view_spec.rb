a12cbe4021b5a6df5ebe7bffb6f1cce446c08eb0
Quinn Lee <leekeunhwi@gmail.com>
2013-06-18 15:10:09 -0400

    adding more tests

--- spec/features/record_user_movie_view_spec.rb
+++ spec/features/record_user_movie_view_spec.rb
@@ -13,8 +13,22 @@ feature "record movie#show that the user views" do
     fill_in 'Email', :with => user.email
     fill_in 'Password', :with => user.password
     click_button("Sign in")
-    save_and_open_page
     click_link(movie.title)
+    expect(user.movies.count).to eql 1
+  end
+
+  scenario "a user visits 1 movie#show pages several times" do
+    visit(root_path)
+    click_link("Login")
+    fill_in 'Email', :with => user.email
+    fill_in 'Password', :with => user.password
+    click_button("Sign in")
+    click_link(movie.title)
+    visit(root_path)
+    click_link(movie.title)
+    visit(root_path)
+    click_link(movie.title)
+    visit(root_path)
     expect(user.movies.count).to eql 1
   end
 
